357afddfda64670c3ac862e59b90c8818ac735b2
sepfy <sepfy95@gmail.com>
2024-07-06 12:45:36 +0800

    Add player config

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
 <div id="container">
+  <div class="btn-setup" id="btn-setup">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/></svg>
+  </div>
   <div class="btn-fs" id="btn-fs">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"/></svg>
   </div>
@@ -12,6 +15,20 @@
   <video ref="video" class="video" playsinline>
     <source src=http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4 type=video/mp4>
   </video>
+  <dialog id="setup-dialog">
+    <textarea id="config" cols=40 rows=10></textarea>
+    <br>
+    <div id="btn-setup-save" class="btn">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V173.3c0-17-6.7-33.3-18.7-45.3L352 50.7C340 38.7 323.7 32 306.7 32H64zm0 96c0-17.7 14.3-32 32-32H288c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V128zM224 288a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+    </div>
+    <div id="btn-setup-edit" class="btn">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>
+    </div>
+    <div id="btn-setup-close" class="btn">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg>
+    </div>
+  </dialog>
+
 </div>
 
 </template>
@@ -23,11 +40,23 @@ import nipplejs from 'nipplejs'
 
 const video = ref(null)
 
-const client = mqtt.connect('wss://broker.emqx.io:8084/mqtt')
+var playerConfig = JSON.parse(localStorage.getItem('pp'))
+if (playerConfig == null) {
+  playerConfig.mqttUrl = "wss://broker.emqx.io:8084/mqtt"
+  playerConfig.deviceId = "test"
+  playerConfig.username = ""
+  playerConfig.password = ""
+}
+
+const deviceId = playerConfig.deviceId
+const client = mqtt.connect(playerConfig.mqttUrl, {
+  clientId: playerConfig.clientId,
+  username: playerConfig.username,
+  password: playerConfig.password,
+})
 
 var offerId = Math.floor((Math.random() * 1000) + 1);
 var answerId = Math.floor((Math.random() * 1000) + 1);
-var deviceId = 'test'
 var isMuted = true
 var dcOpened = false
 
@@ -49,7 +78,11 @@ client.on('connect', function () {
 client.on('message', function (topic, message) {
 
   let msg = JSON.parse(message.toString())
-  if (msg.id == offerId) {
+  console.log(msg)
+
+  if (msg.error != undefined) {
+    console.log(msg.error)
+  } else if (msg.id == offerId) {
 
     let sdp = msg.result;
     let offer = {type: 'offer', sdp: sdp};
@@ -144,6 +177,44 @@ onMounted(() => {
     }
   })
 
+  const setup = document.getElementById('btn-setup')
+  setup.addEventListener('click', () => {
+    document.getElementById('setup-dialog').showModal();
+  });
+
+  const setupEdit = document.getElementById('btn-setup-edit');
+  const setupClose = document.getElementById('btn-setup-close');
+  const setupSave = document.getElementById('btn-setup-save');
+  const configText = document.getElementById('config');
+  configText.value = JSON.stringify(playerConfig, null, 2);
+  configText.disabled = true
+  setupSave.style.display = 'none'
+
+  setupEdit.addEventListener('click', () => {
+    configText.disabled = false
+    setupEdit.style.display = 'none'
+    setupSave.style.display = 'inline-block'
+  });
+
+  setupSave.addEventListener('click', () => {
+
+    try {
+      playerConfig = JSON.parse(document.getElementById('config').value)
+      localStorage.setItem('pp', JSON.stringify(playerConfig));
+      configText.value = JSON.stringify(playerConfig, null, 2);
+      configText.disabled = true
+      setupSave.style.display = 'none'
+      setupEdit.style.display = 'inline-block'
+    } catch {
+      console.log('not json format')
+    }
+
+  });
+
+  setupClose.addEventListener('click', () => {
+    document.getElementById('setup-dialog').close();
+  });
+
   var fs = document.getElementById('btn-fs');
   var mute = document.getElementById('btn-mute');
   fs.addEventListener('click', goFullScreen);
@@ -213,6 +284,31 @@ function exitFullscreen() {
   left: 60px;
 }
 
+.btn-setup {
+  background: rgba(255, 255, 255, 0.3);
+  position: absolute;
+  width: 25px;
+  height: 25px;
+  padding: 10px;
+  border-radius: 15px;
+  z-index: 10;
+  cursor: pointer;
+  bottom: 40px;
+  left: 110px;
+}
+
+.btn {
+  background: rgba(255, 255, 255, 0.3);
+  width: 17px;
+  height: 17px;
+  padding: 10px;
+  color: white;
+  border-radius: 15px;
+  cursor: pointer;
+  display: inline-block
+}
+
+
 .btn-fs {
   background: rgba(255, 255, 255, 0.3);
   position: absolute;
@@ -244,4 +340,11 @@ function exitFullscreen() {
   left: 50%;
   transform: translate(-50%,-50%);
 }
+
+:modal {
+  background-color: beige;
+  border: 2px solid burlywood;
+  border-radius: 5px;
+}
+
 </style>
